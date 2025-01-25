c3052d33d53427d24a18cd635bae81148b0d9911
owrkl <adnanroomieh@gmail.com>
2025-01-25 20:22:44 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 import os
 app = Flask(__name__)
-if __name__ == "__main__"
-	port = int(os.environ.get("PORT",10000))
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT",10000))
   app.run(host="0.0.0.0", port=port)
 import telebot
 
